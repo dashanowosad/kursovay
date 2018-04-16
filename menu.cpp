6464a9fd95222ceae3541eea7be11c23c9046743
aurelialacroixip <denchik.kiselevchik@gamil.com>
2018-04-16 21:03:36 +0700

    Adding menu with cases

---
+++ menu.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{ 
+	setlocale(LC_ALL,"Rus");
+
+    	char c;
+
+
+
+    	while(c!='3')
+    {
+    	cout<<"1. Правила"<<endl;
+    	cout<<"2. Начать игру"<<endl;
+    	cout<<"3. Выход из игры"<<endl;
+    	cin>>c;
+    switch(c)
+    { case '1':
+    {
+    cout<<"Здесь будут правила"<<endl;
+
+
+
+    }
+    case '2':
+    {
+
+
+    }
+
+    }
+
+    }
+
+
+    return 0;
+    }
+
+
