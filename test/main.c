bce84a39d879d2a01ad726d198a1bb8ce9ee1aad
Dasha <dashanowosad@yandex.ru>
2018-04-22 15:05:57 +0700

    Creat test whith ctest lib

---
+++ test/main.c
@@ -0,0 +1,14 @@
+#define CTEST_MAIN
+
+#include <functions.h>
+#include <ctest.h>
+
+
+
+
+int main(int argc, const char** argv) 
+{
+    int test_res = ctest_main(argc, argv);
+    return test_res;
+}
+
