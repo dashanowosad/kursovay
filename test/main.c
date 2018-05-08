135c27a22fde1c67b4161579b2df4ed6934003f6
aurelialacroixip <albinawolf0@gmail.com>
2018-05-08 15:25:01 +0700

    Create prow test

--- test/main.c
+++ test/main.c
@@ -3,7 +3,18 @@
 #include <functions.h>
 #include <ctest.h>
 
-
+CTEST(Prow, Correct_Prow)
+{ int A[17], i,j=0;
+	for (i=1; i<16; i++)
+	{
+	A[j]=i;
+	 j++;
+	}	
+A[15]=0;
+int result = prow(A);
+int expect = 1;
+ASSERT_EQUAL(expect,result);	
+}
 
 
 int main(int argc, const char** argv) 
