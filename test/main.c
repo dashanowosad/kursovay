4ae819f2b458b3d6c58cbc79a68535b79a8a8817
aurelialacroixip <albinawolf0@gmail.com>
2018-05-11 12:28:40 +0700

    Adding incorrect prow test

--- test/main.c
+++ test/main.c
@@ -17,6 +17,23 @@ ASSERT_EQUAL(expect,result);
 }
 
 
+CTEST(Prow, Incorrect_Prow)
+{ int A[17], i,j=0;
+	for (i=1; i<16; i++)
+	{
+	A[j]=i;
+	 j++;
+	}	
+A[15]=0;
+A[13]=15;
+A[14]=14;
+
+int result = prow(A);
+int expect = 0;
+ASSERT_EQUAL(expect,result);	
+}
+
+
 int main(int argc, const char** argv) 
 {
     int test_res = ctest_main(argc, argv);
